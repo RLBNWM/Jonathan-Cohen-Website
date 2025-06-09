42467eb78486a10a03dceb79c4f9fdcf76dd34b1
Ross-Beukes <112417997+Ross-Beukes@users.noreply.github.com>
2025-06-09 10:50:20 +0200

    updates to styling and major sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,23 @@
 
 
+
 fetch("/navbar.html")
     .then(result => {
         return result.text();
 
     }).then(html => {
         document.getElementById('nav-bar-div').innerHTML = html;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+fetch('/footer.html')
+    .then(result => {
+        return result.text();
+    })
+    .then(html => {
+        document.getElementById('footer-div').innerHTML = html;
     })
     .catch(error => {
         console.log(error);
