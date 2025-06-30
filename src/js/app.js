623e6e14487844b3269d7f4aea1462fa61f2d7ec
Ross-Beukes <112417997+Ross-Beukes@users.noreply.github.com>
2025-06-30 17:55:33 +0200

    enabling swiper.js carousels

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,5 @@
 
 
-function main() {
-    var service_swiper = new Swiper(".services-section-scroller", {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            992: { slidesPerView: 3 },
-            768: { slidesPerView: 2 },
-            576: { slidesPerView: 1 }
-        }
-    });
-
-    var media_swiper = new Swiper(".media-resource-scroller", {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            992: { slidesPerView: 3 },
-            768: { slidesPerView: 2 },
-            576: { slidesPerView: 1 }
-        }
-    })
-
-
-}
-
 fetch("/navbar.html")
     .then(result => {
         return result.text();
@@ -63,6 +21,3 @@ fetch('/footer.html')
     .catch(error => {
         console.log(error);
     })
-
-
-
