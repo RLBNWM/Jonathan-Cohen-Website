f6a4f19ebce8ed0061d61b595e3438d3cbcafccb
Ross-Beukes <112417997+Ross-Beukes@users.noreply.github.com>
2025-10-01 10:12:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rlbnwm.github.io/Jonathan-Cohen-Website/',
+  base: '/Jonathan-Cohen-Website/',
   build: {
     outDir: 'dist',
     sourcemap: true
