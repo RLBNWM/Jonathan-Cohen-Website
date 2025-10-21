06f32802d4f0cb5ea6f3fcd113e50b6f35f96be4
Ross-Beukes <112417997+Ross-Beukes@users.noreply.github.com>
2025-10-21 12:02:28 +0200

    linking socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,14 @@
 export default function Footer() {
 
-    const preventDef = (e) => {
-        e.preventDefault();
-    }
 
     return (
         <footer id="footer" className="footer">
             <div className="container">
                 <div className="social-links d-flex justify-content-center">
-                    <a href="" onClick={preventDef}><i className="bi bi-twitter-x"></i></a>
-                    <a href="" onClick={preventDef}><i className="bi bi-facebook"></i></a>
-                    <a href="" onClick={preventDef}><i className="bi bi-instagram"></i></a>
-                    <a href="" onClick={preventDef}><i className="bi bi-linkedin"></i></a>
+                    {/* <a href="" onClick={preventDef}><i className="bi bi-twitter-x"></i></a> */}
+                    {/* <a href="" onClick={preventDef}><i className="bi bi-facebook"></i></a> */}
+                    <a href="https://www.instagram.com/jonathan_cohen_holisticcoach?igsh=dnpvYXRmdW53ZG42&utm_source=qr"><i className="bi bi-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/cohenjonathan?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i className="bi bi-linkedin"></i></a>
                 </div>
             </div>
         </footer>
